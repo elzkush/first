7996c8a581661ca7fbe2f72b67afe4cfe85e94dd
elza <elzada.kushbakova@mail.ru>
2022-06-16 18:07:08 +0600

    first commit

---
+++ First/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Elzada Kushbakova on 16/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
